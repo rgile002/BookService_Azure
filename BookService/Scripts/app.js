e59322236892e7818dd191c6830c2b2dd744109a
Rolf Gilet <rolf.gilet@ggainc.com>
2019-07-19 16:03:01 -0400

    add javascript code for the app

---
+++ BookService/Scripts/app.js
@@ -0,0 +1,31 @@
+﻿var ViewModel = function () {
+    var self = this;
+    self.books = ko.observableArray();
+    self.error = ko.observable();
+
+    var booksUri = '/api/books/';
+
+    function ajaxHelper(uri, method, data) {
+        self.error('');
+        return $.ajax({
+            type: method,
+            url: uri,
+            dataType: 'json',
+            contentType: 'application/json',
+            data: data ? JSON.stringify(data) : null
+        }).fail(function (jqXHR, textStatus, errorThrown) {
+            self.error(errorThrown);
+        });
+    }
+
+    function getAllBooks() {
+        ajaxHelper(booksUri, 'GET').done(function (data) {
+            self.books(data);
+        });
+    }
+
+    // get data on load
+    gettAllBooks();
+};
+
+ko.applyBindings(new ViewModel());
